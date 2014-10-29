9d4f3c928c51b372ebad82e444a9af9e2e1958e7
NanyiJiang <nyjiang@uwaterloo.ca>
2014-10-28 21:16:43 -0700

    Add skybox, start working on flight control

---
+++ Assets/SkyboxFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyboxFollow : MonoBehaviour {
+	
+	public GameObject following;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (following != null) {
+			this.transform.position = following.transform.position;
+		}
+	}
+}
--- Assets/SpaceShipControl.cs
+++ Assets/SpaceShipControl.cs
@@ -2,12 +2,16 @@
 using System.Collections;
 
 public class SpaceShipControl : MonoBehaviour {
+	public Vector3 acceleration;
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
-	
+
+	void FixedUpdate () {
+	}
+
 	// Update is called once per frame
 	void Update () {
 	
