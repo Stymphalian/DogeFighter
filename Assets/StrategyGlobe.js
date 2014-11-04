b0f1743dcbb7a707db43a8da62c92cece00bbf3c
Leon Jiang <titangate1202@gmail.com>
2014-11-03 18:35:51 -0800

    Add strat globe

---
+++ Assets/StrategyGlobe.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+public var reference : GameObject;
+
+function Start () {
+	renderer.material.SetColor("_TintColor", Color(0.0, 1.0, 0.2, 0.1));
+}
+
+function Update () {
+	this.transform.localEulerAngles = -reference.transform.eulerAngles;
+}
