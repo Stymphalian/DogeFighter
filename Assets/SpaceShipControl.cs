0dd563fa9bb90839daa5bff56e319eede1303823
Jordan Yu <jordanyu@athinkingape.com>
2014-12-10 11:35:43 -0800

    fix more bugs

--- Assets/SpaceShipControl.cs
+++ Assets/SpaceShipControl.cs
@@ -51,7 +51,7 @@ public class SpaceShipControl : MonoBehaviour {
 		float yaw = Input.GetAxis("Horizontal");
 		float pitch = -Input.GetAxis("Vertical");
 		float roll = Input.GetAxis("Roll");
-		Debug.Log (roll);
+//		Debug.Log (roll);
 
 		float gas = Input.GetAxis("Gas");
 		if (gas > 0) {
