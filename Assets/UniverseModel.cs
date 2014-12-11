eeeb88274e8539e7beb63e5a2589c9b6e0f59db7
NanyiJiang <nyjiang@uwaterloo.ca>
2014-12-11 09:40:45 -0800

    Universe model contraint

--- Assets/UniverseModel.cs
+++ Assets/UniverseModel.cs
@@ -15,7 +15,8 @@ public class UniverseModel : MonoBehaviour {
 
 	private bool zoomed = true;
 	public float targetScale;
-	float scaleToRangeRatio;
+	public float globezoomLimit = 175;
+	public float scaleToRangeRatio;
 	// Use this for initialization
 	void Start () {
 		targetScale = globe.transform.localScale.x;
@@ -39,18 +40,25 @@ public class UniverseModel : MonoBehaviour {
 			GameObject obj = host.transform.GetChild(i).gameObject;
 			Destroy(obj);
 		}
+		globe.transform.localScale = Vector3.Lerp (globe.transform.localScale, Vector3.one * targetScale, Time.deltaTime);
+		float currentScale = globe.transform.localScale.x;
+		visionRange = scaleToRangeRatio / currentScale;
 		GameObject[] planets = GameObject.FindGameObjectsWithTag ("Planet");
 		foreach (GameObject planet in planets) {
 			Vector3 relativePosition = (planet.transform.position - ship.transform.position);
-			if (relativePosition.magnitude < visionRange) {
+			if (relativePosition.magnitude > globezoomLimit) {
+				relativePosition = relativePosition.normalized * globezoomLimit;
+			}
 				GameObject model = (GameObject)GameObject.Instantiate(planetModel);
 				model.transform.parent = host.transform;
 				model.transform.localPosition = Quaternion.Inverse(ship.transform.rotation) * (relativePosition / visionRange);
-			}
+
 		}
 		if (enemyShip) {
 			Vector3 relativePosition = (enemyShip.transform.position - ship.transform.position);
-			
+			if (relativePosition.magnitude > globezoomLimit) {
+				relativePosition = relativePosition.normalized * globezoomLimit;
+			}
 			GameObject model = (GameObject)GameObject.Instantiate(shipModel);
 			model.transform.parent = host.transform;
 			model.transform.localPosition = Quaternion.Inverse(ship.transform.rotation) * (relativePosition / visionRange);
@@ -59,9 +67,6 @@ public class UniverseModel : MonoBehaviour {
 			line.SetPosition(0, model.transform.position);
 		}
 		line.SetPosition (1, globe.transform.position);
-		globe.transform.localScale = Vector3.Lerp (globe.transform.localScale, Vector3.one * targetScale, Time.deltaTime);
-		float currentScale = globe.transform.localScale.x;
-		visionRange = scaleToRangeRatio / currentScale;
 		Vector3 pos = transform.localPosition;
 		pos.y = currentScale * 0.046f;
 		transform.localPosition = pos;
