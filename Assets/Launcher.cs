01e322a651796e7d567fddf1e1a6c5f50d3f28d4
Ginelle Gaisano <ggaisano@athinkingape.com>
2014-12-11 11:03:30 -0800

    Adding sliding door and launching from box

---
+++ Assets/Launcher.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Launcher : MonoBehaviour {
+	private bool launch;
+	private int currentLaunchSpeed;
+
+	// Use this for initialization
+	void Start () {
+		currentLaunchSpeed = 10;
+		launch = false;
+		StartCoroutine (delayLaunch(1f));
+	
+	}
+
+	IEnumerator delayLaunch (float duration) {
+		yield return new WaitForSeconds (duration);
+		launch = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnTriggerStay(Collider other) {
+		Debug.Log ("INSIDE THE BOX");
+		if (launch) {
+			if (other.attachedRigidbody) {
+				Debug.Log(other.name +  " has rigidbody");
+				other.attachedRigidbody.AddForce(Vector3.right * currentLaunchSpeed);
+				currentLaunchSpeed += 2;
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		currentLaunchSpeed = 10;
+		launch = false;
+	}
+}
---
+++ Assets/SlidingDoor.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlidingDoor : MonoBehaviour {
+
+	private Vector3 openPosition;
+	private Vector3 closedPosition;
+	private bool opened;
+	// Use this for initialization
+	void Start () {
+		openPosition = new Vector3 (transform.position.x, transform.position.y + 50, transform.position.z);
+		closedPosition = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+		opened = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider other){
+		Debug.Log("Trigger open door");
+//		transform.position = Vector3.Lerp(transform.position, openPosition, 0.1f);
+		if (!opened)
+			StartCoroutine (moveDoor());
+	}
+
+	IEnumerator moveDoor(){
+		opened = true;
+		float time = 0;
+		while (time < 1) {
+			transform.position = Vector3.Lerp(transform.position, openPosition, (float) 4*Time.deltaTime);
+			yield return new WaitForSeconds(Time.deltaTime);
+			time += Time.deltaTime;
+		}
+		yield return new WaitForSeconds(1.0f);
+
+		time = 0;
+		while (time < 1) {
+			transform.position = Vector3.Lerp(transform.position, closedPosition, (float) 4*Time.deltaTime);
+			yield return new WaitForSeconds(Time.deltaTime);
+			time += Time.deltaTime;
+		}
+		transform.position = closedPosition;
+	} 
+}
