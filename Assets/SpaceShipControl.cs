ba8057a5de80991015048d533049889b8a667021
Ginelle Gaisano <ggaisano@athinkingape.com>
2014-12-10 23:04:50 -0800

    Disabling user controls on death and displaying proper health

--- Assets/SpaceShipControl.cs
+++ Assets/SpaceShipControl.cs
@@ -89,7 +89,7 @@ public class SpaceShipControl : MonoBehaviour {
 	}
 
 	void FixedUpdate () {
-		if( networkView.isMine == false){return;}
+		if( networkView.isMine == false || deadFlag){return;}
 
 		float yaw = Input.GetAxis("Horizontal");
 		float pitch = -Input.GetAxis("Vertical");
@@ -113,7 +113,7 @@ public class SpaceShipControl : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (networkView.isMine == false) {return;}
+		if (networkView.isMine == false || deadFlag) {return;}
 
 		timeElapsedSinceLastBulletFire += Time.deltaTime;
 		if (Input.GetKeyDown("0")) {
@@ -175,14 +175,16 @@ public class SpaceShipControl : MonoBehaviour {
 		if( networkView.isMine){
 			Debug.Log("My health");
 			this.health = newHealth;
-			healthText.text = this.health.ToString();
-			if( this.health <= 0){
+			if( this.health <= 0 && !deadFlag){
 				this.health = 0;
+				healthText.text = this.health.ToString();
 				deadFlag = true;
 				Debug.Log("You died!");
 				setCockpitMessage("YOU DIED.");
-//				DemoSceneManager.instance.incrementScore(Network.player);
+				DemoSceneManager.instance.reduceLives(Network.player);
 				StartCoroutine(delayRespawn());
+			} else if (this.health > 0) {
+				healthText.text = this.health.ToString();
 			}
 
 			networkView.RPC("updateHealth",RPCMode.Others, newHealth);
