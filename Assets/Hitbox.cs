d310fd281a6b02069ac099d97e659971f1403d39
Chester Lin <clin@athinkingape.com>
2014-12-11 12:16:36 -0800

    Targeting test

---
+++ Assets/Hitbox.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hitbox : MonoBehaviour {
+
+	public GameObject ship;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
